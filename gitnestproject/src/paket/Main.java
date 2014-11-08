1dc0ba9d3ae44b654973aff85be86331ca623559
mur <mur76@list.ru>
2014-11-08 17:35:57 +0400

    add !

--- gitnestproject/src/paket/Main.java
+++ gitnestproject/src/paket/Main.java
@@ -3,7 +3,7 @@ package paket;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(" Hello from git hub suka!");
+		System.out.println(" Hello from git hub suka!!");
 
 	}
 
